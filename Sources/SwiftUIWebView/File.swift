44235795dbb479b0ff26fa3e9479c1623499b808
devanshuchaudhary <52855516+devdchaudhary@users.noreply.github.com>
2023-05-23 15:13:06 +0530

    l

---
+++ Sources/SwiftUIWebView/File.swift
@@ -0,0 +1,42 @@
+//
+//  File.swift
+//  
+//
+//  Created by devdchaudhary on 23/05/23.
+//
+
+import SwiftUI
+
+public func isReachable(urlString: String, completion: @escaping (Bool) -> ()) {
+    
+    guard let url = URL(string: urlString) else {
+        
+        completion(false)
+        return
+    }
+    
+    var request = URLRequest(url: url)
+    
+    request.httpMethod = "HEAD"
+    
+    URLSession.shared.dataTask(with: request) { _, response, _ in
+
+        let status = response as? HTTPURLResponse
+        
+        if status?.statusCode == 200 {
+            
+            completion(true)
+        }
+        
+        let alert = UIAlertController(title: "Invalid URL", message: "This website doesn't exist.", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: { _ in
+            
+        }))
+        
+        UIApplication.shared.windows.first?.rootViewController?.present(alert, animated: true, completion: nil)
+        completion(false)
+    }
+    .resume()
+    
+}
--- Sources/SwiftUIWebView/SwiftUIView.swift
+++ Sources/SwiftUIWebView/SwiftUIView.swift
@@ -66,40 +66,6 @@ public struct WebView: View {
         
     }
     
-    public func isReachable(urlString: String, completion: @escaping (Bool) -> ()) {
-        
-        guard let url = URL(string: urlString) else {
-            
-            completion(false)
-            return
-        }
-        
-        var request = URLRequest(url: url)
-        
-        request.httpMethod = "HEAD"
-        
-        URLSession.shared.dataTask(with: request) { _, response, _ in
-
-            let status = response as? HTTPURLResponse
-            
-            if status?.statusCode == 200 {
-                
-                completion(true)
-            }
-            
-            let alert = UIAlertController(title: "Invalid URL", message: "This website doesn't exist.", preferredStyle: .alert)
-            
-            alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: { _ in
-                dismissView()
-            }))
-            
-            UIApplication.shared.windows.first?.rootViewController?.present(alert, animated: true, completion: nil)
-            completion(false)
-        }
-        .resume()
-        
-    }
-    
     private func dismissView() {
         presentationMode.wrappedValue.dismiss()
     }
