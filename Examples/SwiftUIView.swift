52d98f6c5dd3e8b869770b08eb67bfe579ef48a9
devanshuchaudhary <52855516+devdchaudhary@users.noreply.github.com>
2023-05-23 15:44:48 +0530

    partial README

---
+++ Examples/SwiftUIView.swift
@@ -0,0 +1,41 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by devdchaudhary on 23/05/23.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+
+    var url = "https://www.devdchaudhary.com"
+        
+    var body: some View {
+        
+        VStack {
+
+            Button {
+                
+                isReachable(urlString: url) { success in
+                    if success {
+                        showSheet.toggle()
+                    }
+                }
+                
+            } label: {
+                Text("Show Sheet")
+            }
+            
+        }
+        .fullScreenCover(isPresented: $showSheet) {
+            WebView(url: url, showProgressBar: true)
+        }
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,29 @@
-# SwiftUIWebView
+# SwiftUIWebView - A UIKit Wrapper for showing WebViews in SwiftUI
 
-A description of this package.
+A UIKit Wrapper for SwiftUI to be able to show users WebViews while staying inside your app using the WebKit Framework.
+
+It uses WKWebView to show this.
+
+Requirements
+
+iOS 14,
+Swift 5.0
+Xcode 13.0+
+
+Installation
+
+Swift Package Manager
+
+To integrate SwiftUIWebView into your Xcode project, specify it in Package Dependancies > Click the "+" button > Copy and paste the URL below:
+
+https://github.com/devdchaudhary/SwiftUIWebView
+
+set branch to "master"
+
+Check SwiftUIWebview
+
+Click Add to Project
+
+Usage
+
+
